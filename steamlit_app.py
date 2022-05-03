eb5fd1619d1a14c3327fd27c96a162b9f1440616
SnowEaviles <104793071+snoweaviles@users.noreply.github.com>
2022-05-02 20:20:38 -0500

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -4,6 +4,6 @@ import streamlit
 streamlit.title('My Mom\s New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text(('Kale,Spinach & Rocket Smoothie')
-streamlit.text(('Hard-Bolied Free-Range Egg')
+streamlit.text('Kale,Spinach & Rocket Smoothie')
+streamlit.text('Hard-Bolied Free-Range Egg')
 
