1c7cdd34ef650de3ee4bea131e984f0f811d3325
SnowEaviles <104793071+snoweaviles@users.noreply.github.com>
2022-05-02 20:23:00 -0500

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -8,3 +8,5 @@ streamlit.text('🥗 Kale,Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Bolied Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
